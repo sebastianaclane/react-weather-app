3e65d7e03e139a6012c75cecd41a4ce1b8452db0
sebastianlane <sebastianaclane@gmail.com>
2019-01-17 15:39:42 -0800

    Some progress with using weather data but still have problems accessing the array

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import cloudyLogo from './cloudy-icon.png';
 import rainyLogo from './rainy-icon.png';
 import snowyLogo from './snowy-icon.png';
 var createReactClass = require('create-react-class');
-//{appid=1f1128a9adbf296b8644ae2ac6f80fa1}
-// https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY
 // https://api.openweathermap.org/data/2.5/forecast?q=Whitehorse,ca&appid=1f1128a9adbf296b8644ae2ac6f80fa1
 
 var Day = createReactClass({
@@ -28,13 +26,57 @@ var ForecastTemp = createReactClass({
     }
 })
 
-var FiveDayForecast = [];
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=Whitehorse,ca&units=metric&appid=1f1128a9adbf296b8644ae2ac6f80fa1')
+const weatherData = 'https://api.openweathermap.org/data/2.5/forecast?q=Whitehorse,ca&units=metric&appid=1f1128a9adbf296b8644ae2ac6f80fa1';
+
+const FiveDayForecast = [];
+// go to fetch documentation on mozilla developer network
+// JAN.17TH: this block of code also works but accessing the elements in FiveDayForecast array after doesn't work for some reason?
+fetch(weatherData)
     .then(response => response.json())
-    .then(myJson => console.log(JSON.stringify(myJson)))
-    // .then(data => FiveDayForecast.push(...data));
+    .then(data => FiveDayForecast.push(data))
+    // .then(data => FiveDayForecast.push(data.list));
+// console.log(FiveDayForecast);
+// console.log(FiveDayForecast.length);
+
+
+function check_array (value) {
+    return Object.prototype.toString.apply(value) === '[object Array]';
+}
+
+console.log(check_array(FiveDayForecast)); // outputs: true
+
+function findMatches(wordToMatch, weatherForecast) {
+    return weatherForecast.filter(dataEntry => {
+      // here we need to figure out if the city or state matches what was searched
+      const regex = new RegExp(wordToMatch, 'gi');
+      // going to return true and show the object if the city name or the state name matches what was searched
+      return dataEntry.dt_txt.match(regex);
+    });
+}
+
+// JAN.17TH: this function and the forecastByDay = displayMatches(); works just fine in the console but apparently doesn't work in the javascript for some reason? WTF?
+var forecastByDay = [];
+function displayMatches() {
+  const matchArray = findMatches("00:00:00", FiveDayForecast);
+  return matchArray;
+}
+forecastByDay = displayMatches();
+
+// for (let i = 0; i < FiveDayForecast.length; i++) {
+//     console.log(i);
+//     console.log(FiveDayForecast[i]);
+// }
+
+// FiveDayForecast.filter(dataEntry => (dataEntry.dt_txt.includes("00:00:00")));
+// const result = FiveDayForecast.filter(dataEntry => (dataEntry.dt_txt == "2019-01-18 00:00:00"));
+// console.log(result);
   
-console.table(FiveDayForecast);
+// console.log(FiveDayForecast);
+// console.log(FiveDayForecast[0]);
+// console.log(FiveDayForecast[6]);
+// console.log(FiveDayForecast[14]);
+// console.log(FiveDayForecast[22]);
+// console.log(FiveDayForecast[30]);
     // .then(myJson => console.log(JSON.stringify(myJson)))
     // .then(json => json[0]
     //     .split('. ')
